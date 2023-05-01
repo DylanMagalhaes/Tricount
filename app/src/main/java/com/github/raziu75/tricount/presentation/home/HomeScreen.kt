39754a8e515ee594eec96cf8f4eb809010385f1a
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2023-05-01 12:55:17 +0200

    [Home] Add title

--- app/src/main/java/com/github/raziu75/tricount/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/github/raziu75/tricount/presentation/home/HomeScreen.kt
@@ -2,16 +2,20 @@ package com.github.raziu75.tricount.presentation.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.raziu75.tricount.R
+import com.github.raziu75.tricount.presentation.common.compose.VerticalSpacer
 import com.github.raziu75.tricount.presentation.home.composable.CountCard
 import com.github.raziu75.tricount.presentation.home.state.UiState
 
@@ -31,8 +35,17 @@ fun HomeScreen(
     Column(
         modifier = modifier
             .background(MaterialTheme.colorScheme.background)
-            .padding(24.dp)
+            .padding(24.dp),
     ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            style = MaterialTheme.typography.headlineMedium,
+            text = stringResource(id = R.string.app_name),
+            textAlign = TextAlign.Center,
+        )
+
+        VerticalSpacer(space = 32.dp)
+
         CountCard(
             title = stringResource(id = R.string.home_participants_card_title),
             count = state.participantCount,
